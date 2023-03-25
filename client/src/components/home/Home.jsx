548c516feba5ec0ea65741f2319970eddc61c88b
Sanjeet76 <3676sanjeetkumargupta@gmail.com>
2023-03-26 04:12:16 +0530

    first commit

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,65 @@
+import React,  { useEffect } from 'react';
+
+import { Box, styled } from '@mui/material';
+
+import NavBar from './NavBar';
+import Banner from './Banner';
+import MidSlide from './MidSlide';
+import MidSection from './MidSection';
+import Slide from './Slide';
+
+import { useSelector, useDispatch } from 'react-redux'; // hooks
+import { getProducts as listProducts } from '../../redux/actions/productAction';
+
+const Component = styled(Box)`
+    padding: 20px 10px;
+    background: #F2F2F2;
+`;
+
+const Home = () => {
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, error } = getProducts;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [dispatch])
+
+    return (
+        <>
+            <NavBar />
+            <Component>
+                <Banner />
+                <MidSlide products={products} />
+                <MidSection />
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
+            </Component>
+        </>
+    )
+}
+
+export default Home;
